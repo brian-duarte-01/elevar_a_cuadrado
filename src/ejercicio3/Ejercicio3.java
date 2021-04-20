fbf22d638b3aaebde05a58d1195d31b8140c82b1
brian0106 <56491425+brian0106@users.noreply.github.com>
2021-04-20 15:38:25 -0600

    version 1.0

---
+++ src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,26 @@
+
+package ejercicio3;
+
+import static java.lang.Math.pow;
+
+
+public class Ejercicio3 {
+
+    
+    public static void main(String[] args) {
+       //declaracion de varaibles
+       int n1;
+       int raiz;
+       //entrada
+       n1=5;
+       //proceso
+       raiz=(int) pow(5,2);
+       //salida
+        System.out.println("el numero elevado al cuadrado es: "+raiz);
+       
+       
+    }
+
+  
+    
+}
